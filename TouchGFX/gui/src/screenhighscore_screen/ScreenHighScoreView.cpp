fe51c09556e4bccdd332437c1a47afd8944d2f2a
minhchau-creator <84848387+minhchau-creator@users.noreply.github.com>
2025-07-04 23:02:50 +0700

    v2

---
+++ TouchGFX/gui/src/screenhighscore_screen/ScreenHighScoreView.cpp
@@ -0,0 +1,16 @@
+#include <gui/screenhighscore_screen/ScreenHighScoreView.hpp>
+
+ScreenHighScoreView::ScreenHighScoreView()
+{
+
+}
+
+void ScreenHighScoreView::setupScreen()
+{
+    ScreenHighScoreViewBase::setupScreen();
+}
+
+void ScreenHighScoreView::tearDownScreen()
+{
+    ScreenHighScoreViewBase::tearDownScreen();
+}
---
+++ TouchGFX/gui/src/screenstart_screen/ScreenStartPresenter.cpp
@@ -0,0 +1,18 @@
+#include <gui/screenstart_screen/ScreenStartView.hpp>
+#include <gui/screenstart_screen/ScreenStartPresenter.hpp>
+
+ScreenStartPresenter::ScreenStartPresenter(ScreenStartView& v)
+    : view(v)
+{
+
+}
+
+void ScreenStartPresenter::activate()
+{
+
+}
+
+void ScreenStartPresenter::deactivate()
+{
+
+}
